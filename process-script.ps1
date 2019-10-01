a4f99ca13e1bcd036bc36361b04984085530ec1a
Andy Kukuc <jeep98.ak@gmail.com>
2019-10-01 01:50:52 -0500

    working on it

--- process-script.ps1
+++ process-script.ps1
@@ -5,24 +5,24 @@ ALSO WRITTEN IN VIM BASH FOR WINDOWS ALL GLORY TO VIM
 #>
 start 'C:\Windows\System32\notepad.exe'
 function run_asshole{
+  -BackgroundColor Black
   cls
   #start 'C:\Windows\System32\notepad.exe'
 while($true){
-  Write-Host 'Simple Powershell script to kill proccsess with a .ps1 script.'`n'By: shad0wmast3r (Andy Kukuc).'`n
+  Write-Host 'Simple Powershell script to kill proccsess.'
+  Write-Host 'By: shad0wmast3r (Andy Kukuc).'`n -ForegroundColor Red -BackgroundColor Black
   $user_specific_process=Read-Host 'Please enter the process name you wish to kill.'`n
   $added_wildcard=$user_specific_process+"*"
   #Write-Host 'Printing Users Input And Adding A Wildcard'$added_wildcard'.' -ForegroundColor Cyan`n
   #Start-Sleep 3
   if((Get-Process -name $added_wildcard -ErrorAction SilentlyContinue) -eq $null){
-    Write-Host `n'ERROR SPELL RIGHT NEXT TIME!' -ForegroundColor Red
-    Write-Host 'Invalid Input '$added_wildcard'.' -ForegroundColor Red
-    Start-Sleep 2
-    cls
+    Write-Host `n'ERROR SPELL RIGHT NEXT TIME!' -ForegroundColor Red -BackgroundColor Black
+    Write-Host 'Invalid Input'$added_wildcard'.'`n -ForegroundColor Red -BackgroundColor Black
   }else{
-    Write-Host 'Killing process with Stop-Process -Name '$user_specific_process'.'`n -ForegroundColor DarkGreen
+    Write-Host `n'Killing process with Stop-Process -Name '$user_specific_process'.' -ForegroundColor Green -BackgroundColor Black
     get-process $added_wildcard
     Stop-Process -Name $added_wildcard -Force
-    Start-Sleep 2
+    Start-Sleep 3
     cls
     }
     }
