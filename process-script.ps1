58a9fd32eb96f8b496b5834587ba4410a138cafa
Andy Kukuc <jeep98.ak@gmail.com>
2019-10-01 01:53:47 -0500

    working on it and doing stuff

--- process-script.ps1
+++ process-script.ps1
@@ -5,7 +5,6 @@ ALSO WRITTEN IN VIM BASH FOR WINDOWS ALL GLORY TO VIM
 #>
 start 'C:\Windows\System32\notepad.exe'
 function run_asshole{
-  -BackgroundColor Black
   cls
   #start 'C:\Windows\System32\notepad.exe'
 while($true){
@@ -16,7 +15,6 @@ while($true){
   #Write-Host 'Printing Users Input And Adding A Wildcard'$added_wildcard'.' -ForegroundColor Cyan`n
   #Start-Sleep 3
   if((Get-Process -name $added_wildcard -ErrorAction SilentlyContinue) -eq $null){
-    Write-Host `n'ERROR SPELL RIGHT NEXT TIME!' -ForegroundColor Red -BackgroundColor Black
     Write-Host 'Invalid Input'$added_wildcard'.'`n -ForegroundColor Red -BackgroundColor Black
   }else{
     Write-Host `n'Killing process with Stop-Process -Name '$user_specific_process'.' -ForegroundColor Green -BackgroundColor Black
