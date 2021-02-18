295dae5b9460cf7ee7cdf269dc57ef516a30df27
Andy Kukuc <jeep98.ak@gmail.com>
2021-02-18 16:51:28 -0600

    have a working program but it only unlocks 1 drive need to fix the other thing. I dont know the array counter refuse to work

--- bitlocker_powershell/unlock.ps1
+++ bitlocker_powershell/unlock.ps1
@@ -1,13 +1,14 @@
 <#Written by Andy 'shad0wMast3r' Kukuc#>
 <#Simple script unlocks your hard drive#>
 
+Clear-Host
 <#So in here we are taking user input and going to save and then convert it into an int#>
 
 <#This will add on the colon to the drive letter since its windows and it will bitch#>
 $colon=":" 
 
  <#This prints the message and reads the input#>
-$number_input=Read-Host 'Please enter number of drives that will be unlocked.'`n
+$number_input=Read-Host 'Please enter number of drives that will be unlocked'
 
 <#convert the string to an Integer#>
 $convert_to_int=[int]$number_input 
@@ -17,36 +18,30 @@ $counter=0;
 
 <#start an array fuck face#>
 $drive_array=@() 
+$password_array=@()
 
 <#Damn while loops for arrays are simple#>
 while($counter -lt $convert_to_int){ 
      <#Reads the drive letter and saves them to an array#>
-    $drive_letter=Read-Host 'Please enter letters of the drives to be unlocked.'`n
-
+    $drive_letter=Read-Host 'Please enter letter of the drives to be unlocked'
+    $capitalize_drive_letter=$drive_letter.ToUpper(); <#Yes it worked#>
     <#Adding the drive letters to an array.#>
     $drive_array+=$drive_letter.ToUpper()+$colon
     
+    <#Take Users Password and pass it along#>
+    $password_input=Read-Host "Enter the password for Drive Letter: $capitalize_drive_letter" -AsSecureString
+    $password_array+=$password_input
+      
     <#Start coutner lol#>
     $counter+=1;
-
-    <#Debugging life
-    $drive_array.length debugging bullshit like always
-    write-host 'Print out the length of the array'#>
 }
+    
+#for some odd reason when passing in the array variable and starting a count the $convert_to_int does not save will fix later
+Unlock-Bitlocker -MountPoint $drive_array[0..$convert_to_int] -Password $password_array[0..$convert_to_int]
 
-<#Debugging life
-write-host 'Out of while loop and prints out values inside the array'
-write-host $drive_array[0..$convert_to_int]
-
-<# THIS IS OLD CODE THAT WILL BE REMOVED JUST NEED TO FIGURE OUT THE SYNTAX BUT IT SHOULDNT BE THAT HARD
-$drive1=ConvertTo-SecureString "Input the password in here" -AsPlainText -Force
-$drive2=ConvertTo-SecureString "Input the password in here" -AsPlainText -Force
-$drive3=ConvertTo-SecureString "Input the password in here" -AsPlainText -Force
-
-$drive_array=@("D:","E:","H:") #set it to the drive letter of the encrypted drive
-$password_array=@($drive1,$drive2,$drive3)
-
-Unlock-Bitlocker -MountPoint $drive_array[0..$convert_to_int] #-Password $password_array[0]
-Unlock-Bitlocker -MountPoint $drive_array[1] -Password $password_array[1]
-Unlock-Bitlocker -MountPoint $drive_array[2] -Password $password_array[2]
-#>
+   <#
+    Debugging life
+    $drive_array.length debugging bullshit like always
+    write-host 'Print out the length of the array'
+    #>
+ 
